7121c0d3383fb786cdbe55e2b1715495b1a796f3
J-JD <jamesjohndoe@protonmail.com>
2024-04-30 00:31:21 +0200

    Listing Notes

---
+++ src/notes.ts
@@ -0,0 +1,20 @@
+// Declare the Object Note:
+export interface Note {
+    id: number;
+    title: string;
+    text: string;
+}
+
+// Declare the array of notes:
+export const NOTES: Note[] = [
+    {
+        id: 1,
+        title: 'Lorem ipsum',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        id: 2,
+        title: 'Shakespeare',
+        text: 'To be, or not to be: that is the question',
+    },
+];
